cb6d0d05c07be3aaa4934f9aba25609447eaf15f
Pratap Vardhan <pratap.vardhan@gramener.com>
2014-05-16 23:48:38 +0530

    Elections 2014 results

---
+++ parliament-elections/election2014/README.md
@@ -0,0 +1,4 @@
+Elections India 2014
+====================
+
+Results related to General Assembly (Lok Sabha) elections 2014 in India.
---
+++ parliament-elections/election2014/eci-constituency-wise.py
@@ -0,0 +1,40 @@
+import os
+import hashlib
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+if not os.path.exists('.cache-ec'):
+    os.makedirs('.cache-ec')
+
+ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36'
+session = requests.Session()
+
+def get(url):
+    '''Return cached lxml tree for url'''
+    path = os.path.join('.cache-ec', hashlib.md5(url).hexdigest() + '.html')
+    if not os.path.exists(path):
+        print url
+        response = session.get(url, headers={'User-Agent': ua})
+        with open(path, 'w') as fd:
+            fd.write(response.text.encode('utf-8'))
+    return BeautifulSoup(open(path), 'html.parser')
+
+def eci(url):
+    soup = get(url)
+    data = soup.find_all('table')[5].find_all('table')[1].find_all('table')[0]
+    state = data.find('td').text[0:-14]
+    result = []
+    for tr in data.find_all('tr')[4:]:
+        cells  = [td.text.strip() for td in tr.find_all('td')]
+        cells += [state,url[-3:]]
+        result.append(cells)
+    return result
+
+codes = ['S0'+str(i) for i in range(1,10)]+['S'+str(i) for i in range(10,29)]+['U0'+str(i) for i in range(1,8)]
+result = []
+for code in codes:
+    url = "http://eciresults.nic.in/statewise"+code+".htm?st="+code
+    result += eci(url)
+
+pd.DataFrame(result).to_csv('eci-constituency-wise.csv', index=False, encoding='utf-8')
