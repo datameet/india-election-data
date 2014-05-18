4bda0cf767b97b8f8a08560d4c42eb9438eba802
Pratap Vardhan <pratapgr8@gmail.com>
2014-05-19 01:26:25 +0530

    Minimize requests

--- parliament-elections/election2014/eci-candidate-wise.py
+++ parliament-elections/election2014/eci-candidate-wise.py
@@ -12,18 +12,19 @@ session = requests.Session()
 
 def get(url):
     '''Return cached lxml tree for url'''
-    path = os.path.join('.cache-ecw', hashlib.md5(url).hexdigest() + '.html')
+    url_e=url.encode('utf-8')
+    path = os.path.join('.cache-ecw', hashlib.md5(url_e).hexdigest() + '.html')
     if not os.path.exists(path):
-        print url
+        print (url)
         response = session.get(url, headers={'User-Agent': ua})
         with open(path, 'w') as fd:
-            fd.write(response.text.encode('utf-8'))
+            fd.write(str(response.text.encode('utf-8')))
     return BeautifulSoup(open(path), 'html.parser')
 
 def eci(url,code,const):
     soup = get(url)
     data = soup.find_all('table')[7].find_all('table')[1]
-    state,constituency = data.find('td').text.split(' - ')
+    state,constituency = data.find('td').text.split(' - ',1)
     result = []
     for tr in data.find_all('tr')[3:]:
         cells  = [td.text.strip() for td in tr.find_all('td')]
@@ -35,10 +36,10 @@ codes = ['S0'+str(i) for i in range(1,10)]+['S'+str(i) for i in range(10,29)]+['
 result = []
 for code in codes:
     for const in range(1,81):
+        print code,const
         url = "http://eciresults.nic.in/Constituencywise"+code+str(const)+".htm?ac="+str(const)
-        try:
-            result += eci(url,code,const)
-        except:
-            pass
+        if get(url).title == None or get(url).title.text != 'Constituencywise-All Candidates':
+            break
+        result += eci(url,code,const)
 
 pd.DataFrame(result).to_csv('eci-candidate-wise.csv', index=False, encoding='utf-8')
